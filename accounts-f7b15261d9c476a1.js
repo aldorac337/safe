a86550d25f8949c3eba47d4ce04a3697b7733c9d
BPI Monitor Worker <worker@bpi-monitor.io>
2025-05-26 10:19:36 +0000

    hiavw3yx4upiccak489rsq0y

---
+++ accounts-f7b15261d9c476a1.js
@@ -0,0 +1,36 @@
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+  [7003],
+  {
+    3902: (e, n, c) => {
+      (window.__NEXT_P = window.__NEXT_P || []).push([
+        '/welcome/accounts',
+        function () {
+          return c(57109);
+        },
+      ]);
+    },
+    57109: (e, n, c) => {
+      'use strict';
+      c.r(n), c.d(n, { default: () => a });
+      var s = c(37876),
+        t = c(77328),
+        _ = c.n(t),
+        r = c(26781),
+        u = c(94937);
+      let a = () =>
+        (0, s.jsxs)(s.Fragment, {
+          children: [
+            (0, s.jsx)(_(), {
+              children: (0, s.jsx)('title', { children: ''.concat(u.Az, ' – My accounts') }),
+            }),
+            (0, s.jsx)(r.A, {}),
+          ],
+        });
+    },
+  },
+  (e) => {
+    var n = (n) => e((e.s = n));
+    e.O(0, [636, 6593, 8792], () => n(3902)), (_N_E = e.O());
+  },
+]);
+//# sourceMappingURL=accounts-f7b15261d9c476a1.js.map
