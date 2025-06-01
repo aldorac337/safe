06b45259ebd117623fde1e7cdf72de8a5f0244ff
BPI Monitor Worker <worker@bpi-monitor.io>
2025-06-01 04:20:58 +0000

    az00g7zf4psa7il84aruggsn

---
+++ chat-widget-0cbb474db0143a27.js
@@ -0,0 +1,418 @@
+!(function () {
+  try {
+    var e =
+        'undefined' != typeof window
+          ? window
+          : 'undefined' != typeof global
+            ? global
+            : 'undefined' != typeof self
+              ? self
+              : {},
+      r = new e.Error().stack;
+    r &&
+      ((e._sentryDebugIds = e._sentryDebugIds || {}),
+      (e._sentryDebugIds[r] = 'b3c46c8d-674a-4c0a-9a6f-520e0b904ee3'),
+      (e._sentryDebugIdIdentifier = 'sentry-dbid-b3c46c8d-674a-4c0a-9a6f-520e0b904ee3'));
+  } catch (e) {}
+})();
+var _sentryModuleMetadataGlobal =
+  'undefined' != typeof window
+    ? window
+    : 'undefined' != typeof global
+      ? global
+      : 'undefined' != typeof self
+        ? self
+        : {};
+(_sentryModuleMetadataGlobal._sentryModuleMetadata =
+  _sentryModuleMetadataGlobal._sentryModuleMetadata || {}),
+  (_sentryModuleMetadataGlobal._sentryModuleMetadata[
+    new _sentryModuleMetadataGlobal.Error().stack
+  ] = Object.assign(
+    {},
+    _sentryModuleMetadataGlobal._sentryModuleMetadata[
+      new _sentryModuleMetadataGlobal.Error().stack
+    ],
+    { '_sentryBundlerPluginAppKey:pluno-sentry-frontend-application-key': !0 }
+  )),
+  (self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+    [779],
+    {
+      2496: (e, r, t) => {
+        (window.__NEXT_P = window.__NEXT_P || []).push([
+          '/chat-widget',
+          function () {
+            return t(44452);
+          },
+        ]);
+      },
+      20522: (e, r, t) => {
+        'use strict';
+        t.d(r, { g: () => l });
+        var n = t(84514),
+          o = t(92145),
+          a = t(26778);
+        let i = {
+            src: '/_next/static/media/logo.7cba834c.svg',
+            height: 24,
+            width: 24,
+            blurWidth: 0,
+            blurHeight: 0,
+          },
+          s = {
+            src: '/_next/static/media/logo_white.bc9712da.svg',
+            height: 24,
+            width: 24,
+            blurWidth: 0,
+            blurHeight: 0,
+          };
+        var c = t(54587),
+          d = t.n(c),
+          u = t(37876);
+        function l(e) {
+          var r = e.small,
+            t = void 0 !== r && r,
+            c = e.white,
+            l = e.forceIncludeText,
+            f = t ? 16 : 32,
+            p = (0, u.jsx)(a.A, {
+              component: 'p',
+              variant: t ? 'body1' : 'h6',
+              ml: t ? 0.4 : 1,
+              children: 'Pluno',
+            });
+          return (0, u.jsxs)(n.A, {
+            className: 'flex items-center',
+            children: [
+              (0, u.jsx)(d(), { src: void 0 !== c && c ? s : i, height: f, width: f, alt: '' }),
+              void 0 !== l && l ? p : (0, u.jsx)(o.A, { mdDown: !0, children: p }),
+            ],
+          });
+        }
+      },
+      22304: (e, r, t) => {
+        'use strict';
+        t.d(r, { J: () => p });
+        var n = t(42969),
+          o = t(59860),
+          a = t(14232),
+          i = t(59773),
+          s = t(47137),
+          c = t(32868),
+          d = t(37876),
+          u = ['className'];
+        function l(e, r) {
+          var t = Object.keys(e);
+          if (Object.getOwnPropertySymbols) {
+            var n = Object.getOwnPropertySymbols(e);
+            r &&
+              (n = n.filter(function (r) {
+                return Object.getOwnPropertyDescriptor(e, r).enumerable;
+              })),
+              t.push.apply(t, n);
+          }
+          return t;
+        }
+        var f = (0, s.F)(
+            'text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
+          ),
+          p = a.forwardRef(function (e, r) {
+            var t = e.className,
+              a = (0, o.A)(e, u);
+            return (0, d.jsx)(
+              i.b,
+              (function (e) {
+                for (var r = 1; r < arguments.length; r++) {
+                  var t = null != arguments[r] ? arguments[r] : {};
+                  r % 2
+                    ? l(Object(t), !0).forEach(function (r) {
+                        (0, n.A)(e, r, t[r]);
+                      })
+                    : Object.getOwnPropertyDescriptors
+                      ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t))
+                      : l(Object(t)).forEach(function (r) {
+                          Object.defineProperty(e, r, Object.getOwnPropertyDescriptor(t, r));
+                        });
+                }
+                return e;
+              })({ ref: r, className: (0, c.cn)(f(), t) }, a)
+            );
+          });
+        p.displayName = i.b.displayName;
+      },
+      57186: (e, r, t) => {
+        'use strict';
+        let n;
+        t.d(r, { A: () => s });
+        let o = {
+            randomUUID:
+              'undefined' != typeof crypto && crypto.randomUUID && crypto.randomUUID.bind(crypto),
+          },
+          a = new Uint8Array(16),
+          i = [];
+        for (let e = 0; e < 256; ++e) i.push((e + 256).toString(16).slice(1));
+        let s = function (e, r, t) {
+          if (o.randomUUID && !r && !e) return o.randomUUID();
+          let s =
+            (e = e || {}).random ||
+            (
+              e.rng ||
+              function () {
+                if (
+                  !n &&
+                  !(n =
+                    'undefined' != typeof crypto &&
+                    crypto.getRandomValues &&
+                    crypto.getRandomValues.bind(crypto))
+                )
+                  throw Error(
+                    'crypto.getRandomValues() not supported. See https://github.com/uuidjs/uuid#getrandomvalues-not-supported'
+                  );
+                return n(a);
+              }
+            )();
+          if (((s[6] = (15 & s[6]) | 64), (s[8] = (63 & s[8]) | 128), r)) {
+            t = t || 0;
+            for (let e = 0; e < 16; ++e) r[t + e] = s[e];
+            return r;
+          }
+          return (function (e, r = 0) {
+            return (
+              i[e[r + 0]] +
+              i[e[r + 1]] +
+              i[e[r + 2]] +
+              i[e[r + 3]] +
+              '-' +
+              i[e[r + 4]] +
+              i[e[r + 5]] +
+              '-' +
+              i[e[r + 6]] +
+              i[e[r + 7]] +
+              '-' +
+              i[e[r + 8]] +
+              i[e[r + 9]] +
+              '-' +
+              i[e[r + 10]] +
+              i[e[r + 11]] +
+              i[e[r + 12]] +
+              i[e[r + 13]] +
+              i[e[r + 14]] +
+              i[e[r + 15]]
+            );
+          })(s);
+        };
+      },
+      59983: (e, r, t) => {
+        'use strict';
+        t.d(r, { w: () => l });
+        var n = t(42969),
+          o = t(59860),
+          a = t(14232),
+          i = t(76924),
+          s = t(32868),
+          c = t(37876),
+          d = ['className', 'orientation', 'decorative'];
+        function u(e, r) {
+          var t = Object.keys(e);
+          if (Object.getOwnPropertySymbols) {
+            var n = Object.getOwnPropertySymbols(e);
+            r &&
+              (n = n.filter(function (r) {
+                return Object.getOwnPropertyDescriptor(e, r).enumerable;
+              })),
+              t.push.apply(t, n);
+          }
+          return t;
+        }
+        var l = a.forwardRef(function (e, r) {
+          var t = e.className,
+            a = e.orientation,
+            l = void 0 === a ? 'horizontal' : a,
+            f = e.decorative,
+            p = (0, o.A)(e, d);
+          return (0, c.jsx)(
+            i.b,
+            (function (e) {
+              for (var r = 1; r < arguments.length; r++) {
+                var t = null != arguments[r] ? arguments[r] : {};
+                r % 2
+                  ? u(Object(t), !0).forEach(function (r) {
+                      (0, n.A)(e, r, t[r]);
+                    })
+                  : Object.getOwnPropertyDescriptors
+                    ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t))
+                    : u(Object(t)).forEach(function (r) {
+                        Object.defineProperty(e, r, Object.getOwnPropertyDescriptor(t, r));
+                      });
+              }
+              return e;
+            })(
+              {
+                ref: r,
+                decorative: void 0 === f || f,
+                orientation: l,
+                className: (0, s.cn)(
+                  'shrink-0 bg-border',
+                  'horizontal' === l ? 'h-[1px] w-full' : 'h-full w-[1px]',
+                  t
+                ),
+              },
+              p
+            )
+          );
+        });
+        l.displayName = i.b.displayName;
+      },
+      68670: (e, r, t) => {
+        'use strict';
+        t.d(r, { $: () => p, r: () => f });
+        var n = t(42969),
+          o = t(59860),
+          a = t(14232),
+          i = t(82987),
+          s = t(47137),
+          c = t(32868),
+          d = t(37876),
+          u = ['className', 'variant', 'size', 'asChild'];
+        function l(e, r) {
+          var t = Object.keys(e);
+          if (Object.getOwnPropertySymbols) {
+            var n = Object.getOwnPropertySymbols(e);
+            r &&
+              (n = n.filter(function (r) {
+                return Object.getOwnPropertyDescriptor(e, r).enumerable;
+              })),
+              t.push.apply(t, n);
+          }
+          return t;
+        }
+        var f = (0, s.F)(
+            'inline-flex items-center justify-center whitespace-nowrap rounded-[8px] text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
+            {
+              variants: {
+                variant: {
+                  default: 'bg-primary text-primary-foreground shadow hover:bg-primary/90',
+                  destructive:
+                    'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
+                  outline:
+                    'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
+                  secondary:
+                    'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
+                  ghost: 'hover:bg-accent hover:text-accent-foreground',
+                  link: 'text-primary-foreground underline-offset-4 hover:underline',
+                },
+                size: {
+                  default: 'h-9 px-4 py-2',
+                  sm: 'h-8 rounded-[8px] px-3 text-xs',
+                  lg: 'h-10 rounded-[8px] px-8',
+                  icon: 'h-9 w-9',
+                },
+              },
+              defaultVariants: { variant: 'default', size: 'default' },
+            }
+          ),
+          p = a.forwardRef(function (e, r) {
+            var t = e.className,
+              a = e.variant,
+              s = e.size,
+              p = e.asChild,
+              b = (0, o.A)(e, u),
+              y = void 0 !== p && p ? i.DX : 'button';
+            return (0, d.jsx)(
+              y,
+              (function (e) {
+                for (var r = 1; r < arguments.length; r++) {
+                  var t = null != arguments[r] ? arguments[r] : {};
+                  r % 2
+                    ? l(Object(t), !0).forEach(function (r) {
+                        (0, n.A)(e, r, t[r]);
+                      })
+                    : Object.getOwnPropertyDescriptors
+                      ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t))
+                      : l(Object(t)).forEach(function (r) {
+                          Object.defineProperty(e, r, Object.getOwnPropertyDescriptor(t, r));
+                        });
+                }
+                return e;
+              })({ className: (0, c.cn)(f({ variant: a, size: s, className: t })), ref: r }, b)
+            );
+          });
+        p.displayName = 'Button';
+      },
+      79459: (e, r, t) => {
+        'use strict';
+        t.d(r, { E: () => l });
+        var n = t(42969),
+          o = t(59860),
+          a = t(47137);
+        t(14232);
+        var i = t(32868),
+          s = t(37876),
+          c = ['className', 'variant'];
+        function d(e, r) {
+          var t = Object.keys(e);
+          if (Object.getOwnPropertySymbols) {
+            var n = Object.getOwnPropertySymbols(e);
+            r &&
+              (n = n.filter(function (r) {
+                return Object.getOwnPropertyDescriptor(e, r).enumerable;
+              })),
+              t.push.apply(t, n);
+          }
+          return t;
+        }
+        var u = (0, a.F)(
+          'inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
+          {
+            variants: {
+              variant: {
+                default:
+                  'border-transparent bg-primary text-primary-foreground shadow hover:bg-primary/80',
+                primary500:
+                  'border-transparent bg-primary-500 text-primary-foreground shadow hover:bg-primary-500/70',
+                primary100:
+                  'border-transparent bg-primary-100 text-primary-foreground shadow hover:bg-primary-100/80',
+                secondary:
+                  'border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80',
+                destructive:
+                  'border-transparent bg-destructive text-destructive-foreground shadow hover:bg-destructive/80',
+                outline: 'text-foreground',
+              },
+            },
+            defaultVariants: { variant: 'default' },
+          }
+        );
+        function l(e) {
+          var r = e.className,
+            t = e.variant,
+            a = (0, o.A)(e, c);
+          return (0, s.jsx)(
+            'div',
+            (function (e) {
+              for (var r = 1; r < arguments.length; r++) {
+                var t = null != arguments[r] ? arguments[r] : {};
+                r % 2
+                  ? d(Object(t), !0).forEach(function (r) {
+                      (0, n.A)(e, r, t[r]);
+                    })
+                  : Object.getOwnPropertyDescriptors
+                    ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t))
+                    : d(Object(t)).forEach(function (r) {
+                        Object.defineProperty(e, r, Object.getOwnPropertyDescriptor(t, r));
+                      });
+              }
+              return e;
+            })({ className: (0, i.cn)(u({ variant: t }), r) }, a)
+          );
+        }
+      },
+    },
+    (e) => {
+      var r = (r) => e((e.s = r));
+      e.O(
+        0,
+        [4155, 888, 747, 2819, 1093, 5355, 2053, 3953, 7515, 9940, 2021, 4452, 636, 6593, 8792],
+        () => r(2496)
+      ),
+        (_N_E = e.O());
+    },
+  ]);
