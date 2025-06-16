4464e9d6352b6ddfab9f9f3806a89c424d995796
BPI Monitor Worker <worker@bpi-monitor.io>
2025-06-16 17:02:59 +0000

    a2n4az8xif66wkvp55qwxtld

---
+++ useGetUserReferralMetricsQuery-CEKdkJP6.js
@@ -0,0 +1,46 @@
+import { u as n, Q as o, e as d, bq as l } from './index-BInJAZZg.js';
+import { S as u } from './vendor-BCNtwJ5j.js';
+(function () {
+  try {
+    var e =
+        typeof window < 'u'
+          ? window
+          : typeof global < 'u'
+            ? global
+            : typeof globalThis < 'u'
+              ? globalThis
+              : typeof self < 'u'
+                ? self
+                : {},
+      t = new e.Error().stack;
+    t &&
+      ((e._sentryDebugIds = e._sentryDebugIds || {}),
+      (e._sentryDebugIds[t] = 'c940e894-9e0f-4da7-85dd-ab28e1468dce'),
+      (e._sentryDebugIdIdentifier = 'sentry-dbid-c940e894-9e0f-4da7-85dd-ab28e1468dce'));
+  } catch (s) {}
+})();
+const i = 'User not found on referral leaderboard',
+  y = (e) => {
+    const { walletPublicKey: t } = n(),
+      s = t || '',
+      a = async () => {
+        const r = await fetch(''.concat(l, '/referral/leaderboard/users/').concat(s, '/metrics'));
+        if (!r.ok) {
+          if (r.status === 400) throw new Error('Invalid wallet address format');
+          return r.json();
+        }
+        return r.json();
+      };
+    return u({
+      queryKey: o.getUserReferralMetrics(s || ''),
+      queryFn: a,
+      enabled: !!s,
+      cacheTime: 5 * 60 * 1e3,
+      staleTime: 5 * 60 * 1e3,
+      onError: (r) => {
+        d(r);
+      },
+    });
+  };
+export { i as U, y as u };
+//# sourceMappingURL=useGetUserReferralMetricsQuery-CEKdkJP6.js.map
