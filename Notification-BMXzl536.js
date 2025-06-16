b8c71d7d0cceb93907c923127e7c4a2886d908eb
BPI Monitor Worker <worker@bpi-monitor.io>
2025-06-16 13:51:48 +0000

    rzszgpz5tu6jezks0ue1ff0v

---
+++ Notification-BMXzl536.js
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+  <meta charset='utf-8' />
+  <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+  <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+  <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+  <link rel='manifest' href='/site.webmanifest' />
+  <meta name='msapplication-config' content='/browserconfig.xml' />
+  <meta name='msapplication-TileColor' content='#0b1a31' />
+  <meta name='theme-color' content='#0b1a31' />
+  <meta content='width=device-width, initial-scale=1, maximum-scale=1' name='viewport' />
+  <meta content='#0b1a31' name='theme-color' />
+  <!-- iOS Status Bar (Light content on dark background) -->
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+  <meta property='og:title' content="Solana' s Borrowing, Lending, Market Making, and Leverage venue. Earn yield on
+    SOL, USDC, JUP, JLP, JitoSOL, and more" />
+  <meta property='og:image' content='/Kamino-URL-Banner.png' />
+  <meta property='og:description' content="Solana' s Borrowing, Lending, Market Making, and Leverage venue. Earn
+    yield on SOL, USDC, JUP, JLP, JitoSOL, and more" />
+  <meta name='description' content="Solana' s Borrowing, Lending, Market Making, and Leverage venue. Earn yield on
+    SOL, USDC, JUP, JLP, JitoSOL, and more" data-react-helmet="true" />
+  <meta http-equiv="Content-Security-Policy" content="frame-ancestors *;">
+  <!--
+manifest.json provides metadata used when your web app is installed on a
+user's mobile device or desktop. See https://developers.google.com/web/fundamentals/web-app-manifest/
+-->
+  <!--  <link href="%PUBLIC_URL%/manifest.json" rel="manifest" />-->
+  <!--
+Notice the use of %PUBLIC_URL% in the tags above.
+It will be replaced with the URL of the `public` folder during the build.
+Only files inside the `public` folder can be referenced from the HTML.
+
+Unlike "/favicon.ico" or "favicon.ico", "%PUBLIC_URL%/favicon.ico" will
+work correctly both with client-side routing and a non-root public URL.
+Learn how to configure a non-root public URL by running `npm run build`.
+-->
+  <title>Kamino | Solana Concentrated Liquidity Layer</title>
+  <script>
+    window.onload = function () {
+      try {
+        var referrerHostname = new URL( document.referrer ).hostname;
+        // if ( referrerHostname !== 'kamino.finance' ) {
+        //   location.href = window.location.href;
+        // }
+      } catch ( e ) { }
+    }
+  </script>
+  <script>
+    var global = global || window;
+  </script>
+
+
+
+  <script type="module" crossorigin src="/assets/index-BInJAZZg.js"></script>
+  <link rel="modulepreload" crossorigin href="/assets/vendor-BCNtwJ5j.js">
+  <link rel="stylesheet" crossorigin href="/assets/index-BPzJOG5r.css">
+</head>
+
+<body>
+  <noscript>Kamino is a Solana DeFi protocol that unifies Lending, Liquidity, and Leverage into a single, secure DeFi
+    product suite.
+
+    You can use Kamino to borrow and lend crypto assets; leverage your SOL staking yield, provide leveraged liquidity to
+    DEXs, and earn automated market making yields. Deploy any Solana assets for yield, including SOL, USDC, USDT, JUP,
+    JLP and more.
+
+    Kamino's product suite is packaged into an industry-leading UX that offers transparent analytics, detailed
+    performance data, and extensive position info.</noscript>
+  <div id='root'></div>
+</body>
+
+
+</html>
