878761a21cf5c3e88a33ff4631f2d60793fecad8
BPI Monitor Worker <worker@bpi-monitor.io>
2025-06-18 01:13:18 +0000

    bp1nfpyzh79ct47tt4s92htz

---
+++ useTermsSignatureIxn-P3OwyT6f.js
@@ -0,0 +1,37 @@
+import { y as r, P as s } from './vendor-CFRnETMC.js';
+import { u as i, aa as o, x as u, eA as d } from './index-DwWh_48g.js';
+(function () {
+  try {
+    var e =
+        typeof window < 'u'
+          ? window
+          : typeof global < 'u'
+            ? global
+            : typeof globalThis < 'u'
+              ? globalThis
+              : typeof self < 'u'
+                ? self
+                : {},
+      t = new e.Error().stack;
+    t &&
+      ((e._sentryDebugIds = e._sentryDebugIds || {}),
+      (e._sentryDebugIds[t] = '21484b12-3aa5-487e-ab84-626d641c6eee'),
+      (e._sentryDebugIdIdentifier = 'sentry-dbid-21484b12-3aa5-487e-ab84-626d641c6eee'));
+  } catch (n) {}
+})();
+function w() {
+  const { walletPublicKey: e } = i(),
+    { signMessage: t } = r(),
+    { kaminoSdk: n } = o();
+  return {
+    getSignatureIxns: async () => {
+      if (!u.isTermsSignatureEnabled) return [];
+      if (!n || !e || !t) throw new Error('Wallet is not connected');
+      if (await n.getUserTermsSignatureState(new s(e))) return [];
+      const a = await t(new TextEncoder().encode(d));
+      return [await n.getUserTermsSignatureIx(new s(e), a)];
+    },
+  };
+}
+export { w as u };
+//# sourceMappingURL=useTermsSignatureIxn-P3OwyT6f.js.map
