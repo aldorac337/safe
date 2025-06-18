211ff1818db7e0aa3b8e13eb33bb6f19416e808d
BPI Monitor Worker <worker@bpi-monitor.io>
2025-06-18 01:26:03 +0000

    l1z8h3w8x0x2wrobhe4wgpro

---
+++ useLeverageMultiplyMetricsQuery-CShURMI-.js
@@ -0,0 +1,77 @@
+var p = Object.defineProperty;
+var a = Object.getOwnPropertySymbols;
+var u = Object.prototype.hasOwnProperty,
+  c = Object.prototype.propertyIsEnumerable;
+var i = (e, t, r) =>
+    t in e ? p(e, t, { enumerable: !0, configurable: !0, writable: !0, value: r }) : (e[t] = r),
+  n = (e, t) => {
+    for (var r in t || (t = {})) u.call(t, r) && i(e, r, t[r]);
+    if (a) for (var r of a(t)) c.call(t, r) && i(e, r, t[r]);
+    return e;
+  };
+var g = (e, t) => {
+  var r = {};
+  for (var s in e) u.call(e, s) && t.indexOf(s) < 0 && (r[s] = e[s]);
+  if (e != null && a) for (var s of a(e)) t.indexOf(s) < 0 && c.call(e, s) && (r[s] = e[s]);
+  return r;
+};
+import { dP as I, dQ as k, k as M, Q as E, e as v } from './index-DwWh_48g.js';
+import { a as L, S as T } from './vendor-CFRnETMC.js';
+(function () {
+  try {
+    var e =
+        typeof window < 'u'
+          ? window
+          : typeof global < 'u'
+            ? global
+            : typeof globalThis < 'u'
+              ? globalThis
+              : typeof self < 'u'
+                ? self
+                : {},
+      t = new e.Error().stack;
+    t &&
+      ((e._sentryDebugIds = e._sentryDebugIds || {}),
+      (e._sentryDebugIds[t] = '2f7691ac-4e18-435b-8071-85b54b1ef61e'),
+      (e._sentryDebugIdIdentifier = 'sentry-dbid-2f7691ac-4e18-435b-8071-85b54b1ef61e'));
+  } catch (r) {}
+})();
+const _ = localStorage.getItem('kaminoStaging') ? k : I,
+  m = ({ marketAddress: e, env: t }) =>
+    L.get(
+      ''.concat(_, '/kamino-market/').concat(e.toString(), '/leverage/metrics?env=').concat(t)
+    ).then(({ data: r }) => r),
+  f = [],
+  y = {};
+function A(e) {
+  const t = y,
+    r = y;
+  return (
+    e.forEach((s) => {
+      const o = s.depositReserve + s.borrowReserve;
+      s.tag === 'Leverage' ? (t[o] = s) : s.tag === 'Multiply' && (r[o] = s);
+    }),
+    { leverageLookupTable: t, multiplyLookupTable: r }
+  );
+}
+const Q = ({ marketAddress: e }) => {
+  const { getMarketByAddress: t } = M(),
+    r = t(e),
+    s = async () => {
+      if (!r) throw new Error('No markets found');
+      return m({ marketAddress: r.getAddress(), env: 'mainnet-beta' });
+    },
+    l = T({
+      queryKey: E.getLeverageMultiplyMetrics(r.getAddress()),
+      queryFn: () => s(),
+      enabled: !!r,
+      onError: (d) => {
+        v(d), console.error('Error: Failed to fetch "Lending Metrics"', d);
+      },
+    }),
+    { data: o } = l,
+    b = g(l, ['data']);
+  return n(n({ data: o || f }, A(o || f)), b);
+};
+export { A as c, m as g, Q as u };
+//# sourceMappingURL=useLeverageMultiplyMetricsQuery-CShURMI-.js.map
---
+++ withLendingPrefetchedMarkets-C9zkBK8v.js
@@ -0,0 +1,55 @@
+var u = Object.defineProperty,
+  g = Object.defineProperties;
+var l = Object.getOwnPropertyDescriptors;
+var s = Object.getOwnPropertySymbols;
+var c = Object.prototype.hasOwnProperty,
+  h = Object.prototype.propertyIsEnumerable;
+var d = (e, t, n) =>
+    t in e ? u(e, t, { enumerable: !0, configurable: !0, writable: !0, value: n }) : (e[t] = n),
+  i = (e, t) => {
+    for (var n in t || (t = {})) c.call(t, n) && d(e, n, t[n]);
+    if (s) for (var n of s(t)) h.call(t, n) && d(e, n, t[n]);
+    return e;
+  },
+  a = (e, t) => g(e, l(t));
+import { j as r } from './vendor-CFRnETMC.js';
+import { F as p } from './index-BR0ixhiw.js';
+import { ea as y, x as b } from './index-DwWh_48g.js';
+(function () {
+  try {
+    var e =
+        typeof window < 'u'
+          ? window
+          : typeof global < 'u'
+            ? global
+            : typeof globalThis < 'u'
+              ? globalThis
+              : typeof self < 'u'
+                ? self
+                : {},
+      t = new e.Error().stack;
+    t &&
+      ((e._sentryDebugIds = e._sentryDebugIds || {}),
+      (e._sentryDebugIds[t] = 'ed195198-1a7a-4230-b997-659562e7e267'),
+      (e._sentryDebugIdIdentifier = 'sentry-dbid-ed195198-1a7a-4230-b997-659562e7e267'));
+  } catch (n) {}
+})();
+const M =
+  (e, t = !0, n = !0) =>
+  (o) => {
+    const { isLoading: f } = y();
+    return n && f && b.isLendingPageEnabled
+      ? t
+        ? r.jsx(p, {})
+        : r.jsx('div', {})
+      : r.jsx(
+          e,
+          a(i({}, o), {
+            'data-sentry-element': 'WrappedComponent',
+            'data-sentry-component': 'WithLendingPrefetchedMarkets',
+            'data-sentry-source-file': 'withLendingPrefetchedMarkets.tsx',
+          })
+        );
+  };
+export { M as w };
+//# sourceMappingURL=withLendingPrefetchedMarkets-C9zkBK8v.js.map
